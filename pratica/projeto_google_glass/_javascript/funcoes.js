aee6b4b88988a2c25ed62a1a04b3a4ecc5fb9114
ravilock <ravi.me.professional@hotmail.com>
2021-04-19 04:47:53 -0300

    Adicionados alguns formulários, vídeos e outras coisas

--- pratica/projeto_google_glass/_javascript/funcoes.js
+++ pratica/projeto_google_glass/_javascript/funcoes.js
@@ -1,37 +1,7 @@
-let defaultLeft = '1250px';
-let defaultTop = '50px';
 function mudaIcone(icone) {
-    document.getElementById("foto").src = icone;
-    if (icone == "_imagens/especificacoes-02.png") {
-        document.getElementById("icone").style.left = '1250px'
-        document.getElementById("icone").style.top = '40px'
-        document.getElementById("foto").style.width = '60%'
-        return 0;
-    }
-    if (icone == "_imagens/fotos.png") {
-        document.getElementById("icone").style.left = '1250px'
-        document.getElementById("icone").style.top = '40px'
-        return 0;
-    }
-    if (icone == "_imagens/multimidia.png") {
-        document.getElementById("icone").style.left = '1300px'
-        document.getElementById("icone").style.top = '15px'
-        return 0;
-    }
-    if (icone == "_imagens/contato.png") {
-        document.getElementById("icone").style.left = '1325px'
-        document.getElementById("icone").style.top = '35px'
-        return 0;
-    }
-    if (icone == "_imagens/home.png") {
-        document.getElementById("icone").style.left = '1310px'
-        document.getElementById("icone").style.top = '15px'
-        return 0;
-    }
-    if (icone == "_imagens/glass-oculos-preto-peq.png") {
-        document.getElementById("icone").style.left = defaultLeft
-        document.getElementById("icone").style.top = defaultTop
-        document.getElementById("foto").style.width = '100%'
-        return 0;
-    }
+    document.getElementById("foto").src = icone
+}
+
+function mudaIFrame(id) {
+    document.getElementById("framespec").src = id
 }
